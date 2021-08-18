a9622bec18ab68f83ad7c6eeaca92152d78dc7d3
Zenakhane <lilliantyiso@gmail.com>
2021-08-18 10:23:56 +0200

    first time

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser')
+const SettingsBill = require("./settings-bill")
+const app = express()
+
+const settingsBill = SettingsBill()
+
+const handlebarSetup = exphbs({
+    partialsDir: "./views/partials",
+    viewPath:  './views',
+    layoutsDir : './views/layouts'
+});
+
+app.engine('handlebars', handlebarSetup);
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+
+app.get('/', function(req, res) {
+    res.render("index", {
+        settings: settingsBill.getSettings(),
+        totals: settingsBill.totals()
+    })
+});
+
+app.post('/settings', function(req, res) {
+ 
+
+ settingsBill.setSettings({
+     callCost:req.body.callCost,
+     smsCost: req.body.smsCost,
+     warningLevel: req.body.warningLevel,
+     criticalLevel: req.body.criticalLevel
+ })
+
+ res.redirect('/')
+});
+
+app.post('/action', function(req, res) {
+
+  settingsBill.recordAction(req.body.actionType)
+    res.redirect('/')
+});
+
+
+app.get('/actions', function(req, res) {
+    res.render('actions', {actions: settingsBill.actionsFor()})
+
+});
+
+app.get('/actions/:type', function(req, res) {
+    const actionType = req.params.actionType
+    res.render('actions', {actions: settingsBill.actionsFor(actionType)})
+});
+
+const PORT = process.env.PORT || 3012
+app.listen(PORT, function() {
+    console.log("App started at port:", PORT)
+});
